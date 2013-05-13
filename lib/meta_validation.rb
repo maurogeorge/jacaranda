27913d5a277db11b4d4934c7ff74caf22336ba5f
Mauro George <maurogot@gmail.com>
2013-05-12 21:32:15 -0300

    treats unknown errors

--- lib/meta_validation.rb
+++ lib/meta_validation.rb
@@ -8,7 +8,15 @@ end
 
 class ActiveRecord::Base
   def self.acts_as_meta_validation
-    include MetaValidation
+    begin
+      include MetaValidation
+    rescue => e
+      if e.kind_of?(MetaValidation::MetaValidationError)
+        raise e
+      else
+        raise MetaValidation::MetaValidationError, "Maybe you found a bug, you got a not expected exception. Report this on https://github.com/maurogeorge/meta_validation/issues"
+      end
+    end
   end
 end
 
