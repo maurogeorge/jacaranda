8aae2653d453d9432644f84df9349921af14e209
Mauro George <maurogot@gmail.com>
2013-05-26 20:45:54 -0300

    added the new rspec syntax

--- spec/meta_validation_spec.rb
+++ spec/meta_validation_spec.rb
@@ -6,21 +6,21 @@ describe MetaValidation do
 
     describe "predicate methods" do
 
-      describe "respond to created methods" do 
-        
+      describe "respond to created methods" do
+
         it "with model with single validation" do
           post = PostWithMetaValidation.create(status: "published")
-          post.respond_to?(:published?).should be_true
+          expect(post.respond_to?(:published?)).to be_true
         end
 
         it "with model with multiple validations" do
           post = PostWithMetaValidationAndTitleValidation.create(title: "Title", status: "published")
-          post.respond_to?(:draft?).should be_true
+          expect(post.respond_to?(:draft?)).to be_true
         end
 
         it "with param scoped" do
           post = PostWithMetaValidationScoped.create(status: "published")
-          post.respond_to?(:status_published?).should be_true
+          expect(post.respond_to?(:status_published?)).to be_true
         end
       end
 
@@ -31,11 +31,11 @@ describe MetaValidation do
         end
 
         it "when is true" do
-          post.should be_published
+          expect(post).to be_published
         end
 
         it "when is false" do
-          post.should_not be_draft
+          expect(post).to_not be_draft
         end
       end
     end
@@ -43,15 +43,15 @@ describe MetaValidation do
     describe "scopes" do
 
       it "have the scopes" do
-        PostWithMetaValidation.respond_to?(:publisheds).should be_true
+        expect(PostWithMetaValidation.respond_to?(:publisheds)).to be_true
       end
 
       it "create scoped scopes" do
-        PostWithMetaValidationScoped.respond_to?(:statuses_publisheds).should be_true
+        expect(PostWithMetaValidationScoped.respond_to?(:statuses_publisheds)).to be_true
       end
 
       it "is a relation" do
-        PostWithMetaValidation.publisheds.should be_kind_of(ActiveRecord::Relation) 
+        expect(PostWithMetaValidation.publisheds).to be_kind_of(ActiveRecord::Relation)
       end
 
       describe "returned values" do
@@ -65,32 +65,32 @@ describe MetaValidation do
         end
 
         it "return correct values" do
-          PostWithMetaValidation.publisheds.should include(post_published)
+          expect(PostWithMetaValidation.publisheds).to include(post_published)
         end
 
         it "not return incorrect values" do
-          PostWithMetaValidation.publisheds.should_not include(post_not_published)
+          expect(PostWithMetaValidation.publisheds).to_not include(post_not_published)
         end
       end
     end
 
     describe "errors" do
-      
+
       it "when included before the validations definitions" do
         expect do
-          class PostWithMetaValidationBeforeValidation < Post 
-            acts_as_meta_validation 
+          class PostWithMetaValidationBeforeValidation < Post
+            acts_as_meta_validation
             validates :status, inclusion: { in: %w{published unpublished draft} }
           end
         end.to raise_error(MetaValidation::MetaValidationError, "Model PostWithMetaValidationBeforeValidation has no validation of inclusion")
       end
- 
+
       it "raise error when has 2 inclusion with same name" do
         expect do
-          class PostWithMetaValidationDuplicateInclusionValue < Post 
+          class PostWithMetaValidationDuplicateInclusionValue < Post
             validates :status, inclusion: { in: %w{published unpublished draft} }
             validates :kind, inclusion: { in: %w{report unpublished draft} }
-            acts_as_meta_validation 
+            acts_as_meta_validation
           end
         end.to raise_error(MetaValidation::MetaValidationError, "The following validators are in more than one field: unpublished and draft")
       end
@@ -100,11 +100,11 @@ describe MetaValidation do
         expect do
           class PostWithUnknownError < Post
             validates :status, inclusion: { in: %w{published unplubished draft} }
-            acts_as_meta_validation 
+            acts_as_meta_validation
           end
         end.to raise_error(MetaValidation::MetaValidationError, "Maybe you found a bug, you got a not expected exception. Report this on https://github.com/maurogeorge/meta_validation/issues")
       end
     end
-  end 
+  end
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,9 @@ require "#{File.dirname(__FILE__)}/support/schema.rb"
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each do |file|
   require file
 end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
